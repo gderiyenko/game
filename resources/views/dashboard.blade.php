491ed079fd45da8527195f73f09ca7d433b1ee77
Gleb Deriyenko <gderiyenko@Glebs-MacBook-Pro.local>
2021-01-14 16:16:47 +0200

    Dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,37 +1,34 @@
 @extends('layouts.app')
 @section('content')
-<main class="py-4">
+<main class="py-4" style="background-image: url('images/game-background.jpg'); background-attachment: fixed;">
   <div class="content">
-    <div class="title m-b-md">devGame</div>
-    <table class="table table-striped" id="myTable" cellspacing="0" width="100%">
-      <thead>
-        <tr>
-          <th class="h2">Emoji</th>
-          <th class="h2">Username</th>
-          <th class="h2">time</th>
-          <th class="h2">mentoring</th>
-          <th class="h2">responsibility</th>
-          <th class="h2">codestyle</th>
-          <th class="h2">sum</th>
-          <th class="h2">Result</th>
-        </tr>
-      </thead>
-      <tbody>
-        @php $i=0; @endphp
-        @foreach ($users as $user)
-        <tr>
-          <td class="h2">{!! $avatars[$i++]['avatar'] !!}</td>
-          <td class="h2">{!! $user['username'] !!}</td>
-          <td class="h2">{!! $user['trackingtime'] !!}</td>
-          <td class="h2">{!! $user['mentoring'] !!}</td>
-          <td class="h2">{!! $user['responsibility'] !!}</td>
-          <td class="h2">{!! $user['codestyle'] !!}</td>
-          <td class="h2">{!! $user['codestyle'] + $user['responsibility'] + $user['mentoring'] !!}</td>
-          <td class="h2">{!! $user['trackingtime'] + $user['codestyle'] + $user['responsibility'] + $user['mentoring'] !!}</td>
-        </tr>
-        @endforeach
-      </tbody>
-    </table>
-  </div>
+
+    <div class="title m-b-md" style="color:yellow">devGame</div>
+    <div class="row">
+
+      @php $i=0; $background_colors = ['7082c8', 'fbc9c8', 'e38d8c', '032547', '0330e0']; @endphp
+      @foreach ($users as $user)
+        @if ($i % 2 == 1)
+        <div class="col pt-5 mt-5">
+        @else
+        <div class="col m">
+        @endif
+        <div class="card m-4" style="color:#<?php echo $background_colors[$i % 3]; ?>; background-color:black">
+          <img src="{{ asset('images/avatars/'. $i++ % 21 . '.png') }}" style="width: 100px">
+          <h1>#{!! $i !!} {!! '@' . $user['username'] !!}</h1>
+          <h4>Tracking Time: {!! $user['trackingtime'] !!}</h4>
+          <h4>Mentoring: {!! $user['mentoring'] !!}</h4>
+          <h4>Responsibility: {!! $user['responsibility'] !!}</h4>
+          <h4>Codestyle: {!! $user['codestyle'] !!}</h4>
+          <h4>Voting sum: {!! $user['codestyle'] + $user['responsibility'] + $user['mentoring'] !!}</h4>
+          <h2>Total: {!! $user['trackingtime'] + $user['codestyle'] + $user['responsibility'] + $user['mentoring'] !!}</h2>
+        </div>
+      </div>
+        @if ($i % 2 == 0)
+        </div>
+        <div class="row">
+        @endif
+      @endforeach
+    </div>
 </main>
 @endsection
